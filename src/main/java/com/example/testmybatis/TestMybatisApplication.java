c53a4d87be1dd4ada819bd85f0bb626c75af36bb
diypcfev <username@email.com>
2022-10-09 16:12:03 +0800

    init

---
+++ src/main/java/com/example/testmybatis/TestMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMybatisApplication.class, args);
+    }
+
+}
